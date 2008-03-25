5986600518047fece5ad5f1c4621110844c7be0e
Patrick Ewing <patrick.henry.ewing@gmail.com>
2008-03-25 17:28:10 -0400

    Undocumented API updates for the lossage (of massive amounts of time)

--- lib/video.rb
+++ lib/video.rb
@@ -1,10 +1,10 @@
 module Viddler
   class Video
-    attr_accessor :id, :url, :title, :description, :tags, :thumbnail_url
-    
+    attr_accessor :upload_time, :permissions, :comment_count, :title, :url, :thumbnail_url, :description, :tags, :author, :length_seconds, :view_count, :id
+        
     def self.post(session, file_data, options = {})
       multipart_file = Net::HTTP::MultipartPostFile.new("temp", 'video/quicktime', file_data)
-      args = {:method => 'viddler.videos.upload', :api_key => session.api_key, :sessionid => session.session_id, :file => multipart_file, :title => "Test"}#.merge!(options)
+      args = {:method => 'viddler.videos.upload', :api_key => session.api_key, :sessionid => session.session_id, :file => multipart_file, :title => "Test"}.merge!(options)
       response = Net::HTTP.post_multipart_form(Viddler.url, args)
       new(Viddler::Parser.parse( args[:method], response ))
     end
@@ -16,5 +16,24 @@ module Viddler
       raise ArgumentError if id.nil? || url.nil?
     end
     
+    
+    def permissions
+      Struct.new(:view_level)
+    end
+  #<permissions>
+  #  <view level="shared_all">
+  #    <secreturl>http://www.viddler.com/explore/username/videos/123/?secreturl=112635379</secreturl>
+  #  </view>
+  #  <embed level="shared">
+  #    <user>friend1</user>
+  #    <user>friend2</user>
+  #    <list>my buddy list1</list>
+  #  </embed>
+  #  <tagging level="shared">
+  #    <list>my buddy list2</list>
+  #  </tagging>
+  #  <commenting level="shared_all" />
+  #  <download level="shared_all" />
+  #</permissions>
   end
 end
